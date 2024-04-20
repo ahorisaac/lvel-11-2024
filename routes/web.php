c13b798e8c3561e6faf46417fe05f2aeba74baea
ahorisaac <ahorisaac.ai@gmail.com>
2024-04-20 19:23:59 +0100

    laravel 11: controllers

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 
-Route::post('/', function (Request $request) {
-    dd($request->all());
-});
-
-Route::put('/{id}', function (Request $request, $id) {
-    return $id;
-});
-
-Route::delete('/{id}', function ($id) {
-    return $id;
-});
-
-Route::get('/test', function () {
-    $test = "Test again";
-    return "<h1>" . $test . "</h1>";
-});
+Route::resource('/posts', PostController::class);
