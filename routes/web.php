1236289ab85ba00c610367c786f6590041ece95c
ahorisaac <ahorisaac.ai@gmail.com>
2024-04-20 17:48:36 +0100

    laravel 11: routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/', function (Request $request) {
+    dd($request->all());
+});
+
+Route::put('/{id}', function (Request $request, $id) {
+    return $id;
+});
+
+Route::delete('/{id}', function ($id) {
+    return $id;
+});
+
+Route::get('/test', function () {
+    $test = "Test again";
+    return "<h1>" . $test . "</h1>";
 });
