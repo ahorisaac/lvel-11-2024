68797bf0774dd5d78e6a42507cc12d8f470ac340
ahorisaac <ahorisaac.ai@gmail.com>
2024-04-27 18:09:31 +0100

    laravel 11: create layout component

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Posts</title>
-</head>
-<body>
+<x-layout>
     <h1>Posts Index Page</h1>
     {{ date('Y-m-d') }}
-</body>
-</html>
+</x-layout>
