915addc59fad13abd3d988129fa0b7547c3a0f33
ahorisaac <ahorisaac.ai@gmail.com>
2024-07-07 18:11:04 +0100

    laravel 11: create registration functionality

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,59 @@
+<x-layout>
+    <x-header>Register</x-header>
+
+    <div class="max-w-2xl mx-auto p-4 bg-slate-200 dark:bg-slate-900 rounded-lg">
+        <form method="POST" action="{{ route('register.store') }}">
+            @csrf
+            {{-- name --}}
+            <div class="mb-6">
+                <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                <input type="text" id="name-input" name="name" placeholder="Ex: Taylor Otwell"
+                    class="@error('name') border-red-500 @enderror bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none">
+                @error('title')
+                    <span class="text-red-500 text-xs my-2"> {{ $message }} </span>
+                @enderror
+            </div>
+
+            {{-- email --}}
+            <div class="mb-6">
+                <label for="base-input"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                <input type="email" id="email-input" name="email" placeholder="treyshi@shidoon.com"
+                    class="@error('name') border-red-500 @enderror bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none">
+                @error('email')
+                    <span class="text-red-500 text-xs my-2"> {{ $message }} </span>
+                @enderror
+            </div>
+
+            {{-- password --}}
+            <div class="mb-6">
+                <label for="base-input"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                <input type="password" id="password-input" name="password" placeholder="Create a strong password"
+                    class="@error('name') border-red-500 @enderror bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none">
+                @error('password')
+                    <span class="text-red-500 text-xs my-2"> {{ $message }} </span>
+                @enderror
+            </div>
+
+            {{-- confirm password --}}
+            <div class="mb-6">
+                <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm
+                    Password</label>
+                <input type="password" id="password-confirmation-input" name="password_confirmation"
+                    placeholder="Create a strong password"
+                    class="@error('name') border-red-500 @enderror bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none">
+                @error('password_confirmation')
+                    <span class="text-red-500 text-xs my-2"> {{ $message }} </span>
+                @enderror
+            </div>
+
+            {{-- submit button --}}
+            <div class="mb-6">
+                <button type="submit"
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Submit
+                </button>
+            </div>
+        </form>
+    </div>
+</x-layout>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,10 +2,12 @@
     <x-header>Posts Index Page</x-header>
 
     <section>
-        <div class="flex justify-end">
-            <a href="{{ route('posts.create') }}"
-                class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Create</a>
-        </div>
+        @auth
+            <div class="flex justify-end">
+                <a href="{{ route('posts.create') }}"
+                    class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Create</a>
+            </div>
+        @endauth
 
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             @foreach ($posts as $post)
