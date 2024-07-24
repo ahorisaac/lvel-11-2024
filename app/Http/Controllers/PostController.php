53a762f1d274f7129a7822f3d3dcd8cdf4b4c650
ahorisaac <ahorisaac.ai@gmail.com>
2024-07-24 13:22:15 +0100

    laravel 11: relationships

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -34,7 +34,7 @@ class PostController extends Controller
             'content' => ['required', 'min:10'],
         ]);
 
-        Post::create($validated);
+        auth()->user()->posts()->create($validated);
 
         return to_route('posts.index');
     }
@@ -52,6 +52,9 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
+        if($post->user_id !== auth()->id()) {
+            abort(403);
+        }
         return view("posts.edit", ["post" => $post]);
     }
 
