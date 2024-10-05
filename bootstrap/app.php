6c6cba2e6db8fb3b5ece56f2d505e41a3e2828d1
ahorisaac <ahorisaac.ai@gmail.com>
2024-10-05 22:26:18 +0100

    laravel 11: task scheduling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,9 +2,12 @@
 
 use App\Http\Middleware\CanViewPostMiddleware;
 use App\Http\Middleware\IsAdminMiddleware;
+use App\Mail\PostCountMail;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Mail;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,6 +18,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias(['can-view-post' => CanViewPostMiddleware::class,]);
         $middleware->alias(['is-admin' => IsAdminMiddleware::class]);
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->call(function () {
+            Mail::to("admin@admin.com")->send(new PostCountMail());
+        })->everyTwoMinutes();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
---
+++ resources/views/emails/post-count.blade.php
@@ -0,0 +1,8 @@
+<x-mail::message>
+# Laravel Tutorial Post Count
+
+You have {{ $postCount }} posts on your database.
+
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message>
