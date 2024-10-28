d42177cb2226ea9d5ec3751f9d0ab09784d661a2
ahorisaac <ahorisaac.ai@gmail.com>
2024-10-28 23:38:45 +0100

    laravel 11: exception handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,10 +4,13 @@ use App\Http\Middleware\CanViewPostMiddleware;
 use App\Http\Middleware\IsAdminMiddleware;
 use App\Mail\PostCountMail;
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Support\Facades\Mail;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -25,5 +28,8 @@ return Application::configure(basePath: dirname(__DIR__))
         })->everyTwoMinutes();
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function(NotFoundHttpException $e, Request $request) {
+            return response()->view("errors.404");
+            // dd($e);
+        });
     })->create();
